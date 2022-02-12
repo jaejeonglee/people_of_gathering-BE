a78ccc0d3e5b841eabe93246e29df67308c7700b
LeeKiGon <dl_rlrhs@naver.com>
2022-02-13 03:35:47 +0900

    post schemas 수정

--- routes/post.js
+++ routes/post.js
@@ -9,7 +9,7 @@ router.use(express.urlencoded({ extended : true }));
 
 router.get('/post', async (req, res) => { //전체 게시글 조회(메인 페이지)
     const { category } = req.query; //카테고리 검색
-    const post = await Post.find({ category });
+    const post = await Posts.find({ category });
     res.json({ post });
 });
 
@@ -23,35 +23,35 @@ router.get('/post/:postId', async (req, res) => { //게시글, 댓글 가져오�
 router.post('/post', async (req, res) => { // 게시글 저장
     const postId = uniqid();
     const { userId, title, userName, createDate, deadLine, category, curMembers, maxMembers, contents  } = req.body;
-    await Post.create({ postId, userId, title, userName, createDate, deadLine, category, curMembers, maxMembers, contents });
+    await Posts.create({ postId, userId, title, userName, createDate, deadLine, category, curMembers, maxMembers, contents });
 
     res.json({ success: "저장이 성공 하였습니다!!"});
 });
 
 router.delete('/delete/:postId', authMiddleware, async (req, res) => { //게시글 삭제
     const { postId } = req.params;
-    await Post.deleteOne({ postId : postId });
+    await Posts.deleteOne({ postId : postId });
     res.json({ success : "삭제가 완료 되었습니다"});
 });
 
 router.patch('/modify/:postId', authMiddleware, async (req, res) => { //게시글 수정
     const { title, deadLine, category, maxMembers, contents } = req.body // 클라이언트에서 전달 받은 수정 할 내용
     const { postId } = req.params;
-    await Post.updateOne({postId}, {$set:{title, deadLine, category, maxMembers, contents}});
+    await Posts.updateOne({postId}, {$set:{title, deadLine, category, maxMembers, contents}});
     res.json({ success : "수정 되었습니다" });
 });
 
 router.post('/join/:postId', authMiddleware, async (req, res) => { //참여인원 추가
     const { postId } = req.params;
     const { curMembers } = req.body;
-    await Post.updateOne({ postId }, { $push : { curMembers }});
+    await Posts.updateOne({ postId }, { $push : { curMembers }});
     res.json({ success : "추가 되었습니다"});
 });
 
 router.patch('/join/:postId', authMiddleware, async (req, res) => { //참여 취소
     const { postId } = req.params;
     const { curMembers } = req.body;
-    await Post.updateOne({ postId }, { $pull: { curMembers }});
+    await Posts.updateOne({ postId }, { $pull: { curMembers }});
     res.json({ success : "참여 취소 되었습니다"});
 });
 
