c40874fa7c0a0980ece0a2f0436f778693c1092a
jaejeonglee <https://github.com/jaejeonglee>
2022-02-13 22:09:18 +0900

    bcrypt 적용, 라우터 요청주소 수정, ejs모듈 제거

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Post = require('./schemas/post');
-const ejs = require('ejs');
 const cors = require('cors');
 
 const postRouter = require('./routes/post');
@@ -32,5 +30,5 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.listen(3000, () => {
-    console.log( new Date().toLocaleString() , '서버가 요청을 받을 준비가 됐어요');
+    console.log( new Date().toLocaleString() , '서버가 3000포트로 요청을 받을 준비가 됐어요');
 });
